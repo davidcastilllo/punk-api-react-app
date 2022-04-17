75ab701b502200375b765e800aad646744552219
DavidCastillo <seth.castillo4@outlook.com>
2022-04-17 01:23:03 -0500

    Finished Bulk of Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import BeerCard from './comps/BeerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      beerData: []
+    }
+  }
+
+  componentDidMount() {
+    this.initDataLoad()
+  }
+
+  initDataLoad = async () => {
+    let qry = (await axios.get('https://api.punkapi.com/v2/beers')).data
+    this.setState({
+      beerData: [...qry]
+    })
+  }
+
+  favorite = () => {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => { console.log(this.state.beerData) }}>Click To Log Beer Info</button>
+        {this.state.beerData.map((beer,index) => {
+          return (
+            <ul>
+              <BeerCard key={index} beer={beer} />
+            </ul>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/comps/BeerCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../comps/BeerCard.css"
+
+function BeerCard(props) {
+  return (
+    <li id="beerCard">
+      <div id="infoBox">
+        <div className="beerName"><b>Name:</b> <br></br>{props.beer.name}</div>
+        <div className="beerLine"><b>Tagline:</b> <br></br><i>{props.beer.tagline}</i></div>
+        <div className="beerDesc"><b>Description:</b> <br></br>{props.beer.description}</div>
+
+      </div>
+      <div id="imgAndButton">
+        <img src={props.beer.image_url} id="beerImg"></img>
+        <button onClick={() => { console.log('Clicked') }} id="beerBtn" >Favorite</button>
+      </div>
+
+    </li>
+  )
+}
+
+export default BeerCard
